d5838462f52fe1d40b94f9668805e1bb15d41370
Jonna C. Geronimo <jonnacgeronimo@gmail.com>
2023-08-11 14:54:04 +0200

    ready for correction
    refactor to arrow function

--- es.js
+++ es.js
@@ -1,13 +1,13 @@
 const fs = require("fs").promises;
 
-async function writeToFile(filePath, content) {
+const writeToFile = async (filePath, content) => {
   try {
     await fs.writeFile(filePath, content);
     console.log("Il file è stato scritto con successo.");
   } catch (err) {
     console.error("Si è verificato un errore nella scrittura del file:", err);
   }
-}
+};
 
 const filePath = "es-out.txt";
 const content = "ciao 1234";
