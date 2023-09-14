58ab8ad82dfd298389d9be1aceb06f6fcafc3987
Jonna C. Geronimo <jonnacgeronimo@gmail.com>
2023-09-14 08:13:28 +0200

    ready for correction

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+require("express-async-errors");
+
+const app = express();
+const morgan = require("morgan");
+const db = require("./db");
+db.setupDb();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+
+const { SERVER_PORT } = process.env;
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server up and running on port ${SERVER_PORT}`);
+});
+
+module.exports = app;
